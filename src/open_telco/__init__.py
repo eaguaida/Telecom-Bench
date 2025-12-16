c8ec0d2f0324b64e74ddb8cf4300dc4e7c86c5a4
Enrique <74030303+eaguaida@users.noreply.github.com>
2025-12-16 17:40:22 +0000

    legacy_3ggp
    - Added extra legacy_3ggp implementation

--- src/open_telco/__init__.py
+++ src/open_telco/__init__.py
@@ -3,8 +3,12 @@
 from . import telelogs, telemath, teleqna
 
 __all__ = [
+    "legacy_3ggp",
+    "legacy_teleyaml",
     "telelogs",
     "telemath",
     "teleqna",
+    "teleyaml",
+    "three_gpp",
 ]
 
